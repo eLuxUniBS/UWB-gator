0d7ece8b8747a82ec5d79b38b106fcf5ff2fbee4
RedsAnDev <code@redsandev.com>
2021-05-16 20:06:10 +0200

    migrazione su docker completata

--- launch_srv_save.sh
+++ launch_srv_save.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python3 main.py --mode cli_server --port $PORT --host $HOST >>./file.log 2>&1
+#python3 main.py --mode cli_server --port $PORT --host $HOST >>./file.log 2>&1
+python3 main.py --mode cli_server --port $PORT --host $HOST
